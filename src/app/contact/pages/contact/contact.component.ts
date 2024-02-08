e1109baabc173deed591c111b2b8b80ba8882dd8
Jose Angel <jose.angel.hdz.rda@gmail.com>
2024-02-08 14:30:29 -0600

    formulario de contacto

--- src/app/contact/pages/contact/contact.component.ts
+++ src/app/contact/pages/contact/contact.component.ts
@@ -1,13 +1,33 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { MainLayoutComponent } from '../../../shared/layouts/MainLayout/MainLayout.component';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [CommonModule, MainLayoutComponent],
+  imports: [
+    CommonModule,
+    MainLayoutComponent,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+  ],
   templateUrl: `./contact.component.html`,
   styleUrl: './contact.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ContactComponent {}
+export class ContactComponent {
+  public form = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    text: ['', [Validators.required]],
+  });
+
+  constructor(private fb: FormBuilder) {}
+
+  submit() {}
+}
