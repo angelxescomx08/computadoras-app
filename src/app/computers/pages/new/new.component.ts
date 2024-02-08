5d8773ec7318679c6f0fadcaa393bf056a3a5246
Jose Angel <jose.angel.hdz.rda@gmail.com>
2024-02-08 11:10:58 -0600

    refactorizacion de código

--- src/app/computers/pages/new/new.component.ts
+++ src/app/computers/pages/new/new.component.ts
@@ -1,11 +1,5 @@
 import { CommonModule } from '@angular/common';
-import {
-  ChangeDetectionStrategy,
-  Component,
-  OnInit,
-  WritableSignal,
-  signal,
-} from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import {
   Validators,
   FormsModule,
@@ -17,8 +11,6 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { ComputersService } from '../../services/computers.service';
 import { MatSelectModule } from '@angular/material/select';
 import { MatButtonModule } from '@angular/material/button';
-import { Router } from '@angular/router';
-import { Computer } from '../../interfaces/computer.interface';
 
 @Component({
   selector: 'app-new',
@@ -47,27 +39,15 @@ export class NewComponent implements OnInit {
 
   constructor(
     public computersService: ComputersService,
-    private router: Router,
     private fb: FormBuilder
   ) {}
 
   ngOnInit(): void {}
 
-  cancel() {
-    this.router.navigate(['/']);
-  }
-
   submit() {
     if (this.form.invalid) {
       return;
     }
-    const computer: Computer = {
-      brand: this.form.value.brand!,
-      price: this.form.value.price!,
-      RAMCapacity: this.form.value.RAMCapacity!,
-      storageCapacity: this.form.value.storageCapacity!,
-      storageType: this.form.value.storageType!,
-    };
-    this.computersService.addComputer(computer);
+    this.computersService.addComputer(this.form);
   }
 }
--- src/app/computers/services/computers.service.ts
+++ src/app/computers/services/computers.service.ts
@@ -1,5 +1,7 @@
 import { Injectable, signal } from '@angular/core';
 import { Computer } from '../interfaces/computer.interface';
+import { Router } from '@angular/router';
+import { FormGroup } from '@angular/forms';
 
 @Injectable({
   providedIn: 'root',
@@ -9,10 +11,22 @@ export class ComputersService {
   public storageTypes = signal(['HDD', 'SSD']);
   public RAMCapacities = signal(['8GB', '16GB', '32GB', '64GB']);
 
-  constructor() {}
+  constructor(private router: Router) {}
 
-  addComputer(computer: Computer) {
+  createComputerByFormGroup(form: FormGroup): Computer {
+    const computer: Computer = {
+      brand: form.value.brand!,
+      price: form.value.price!,
+      RAMCapacity: form.value.RAMCapacity!,
+      storageCapacity: form.value.storageCapacity!,
+      storageType: form.value.storageType!,
+    };
+    return computer;
+  }
+
+  addComputer(form: FormGroup) {
     const computersString = localStorage.getItem('computers');
+    const computer = this.createComputerByFormGroup(form);
     if (!computersString) {
       localStorage.setItem('computers', JSON.stringify([computer]));
       return;
@@ -20,5 +34,14 @@ export class ComputersService {
     const computers: Computer[] = JSON.parse(computersString);
     computers.push(computer);
     localStorage.setItem('computers', JSON.stringify(computers));
+    this.clearForm(form);
+  }
+
+  cancelAction() {
+    this.router.navigate(['/']);
+  }
+
+  clearForm(form: FormGroup) {
+    form.reset();
   }
 }
