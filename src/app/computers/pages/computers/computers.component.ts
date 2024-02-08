2d5b6681345b32466f0b9b6889cb47e3892d8498
Jose Angel <jose.angel.hdz.rda@gmail.com>
2024-02-08 12:23:01 -0600

    filtros

--- src/app/computers/pages/computers/computers.component.ts
+++ src/app/computers/pages/computers/computers.component.ts
@@ -47,4 +47,13 @@ export class ComputersComponent {
     this.dataSource.paginator = this.paginator;
   }
   constructor(public computersService: ComputersService) {}
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
 }
